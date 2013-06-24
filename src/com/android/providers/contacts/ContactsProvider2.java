5c09524be83358ea98a8dfb62f10dc0670bfaf36
Orca Project <shamukillaz@orcaproject.co>
2013-06-23 21:53:23 -0400

    contacts: Privacy Guard support
    * If privacy guard is enabled for the calling process, return an empty
       MemoryCursor (with column names populated) instead of the real
    dataset.
    Change-Id: I78b16f3791706636d51785e46a1f2ec656a3c9d2

--- src/com/android/providers/contacts/ContactsProvider2.java
+++ src/com/android/providers/contacts/ContactsProvider2.java
@@ -736,6 +736,8 @@ public class ContactsProvider2 extends AbstractContactsProvider
     /** Contains just the contacts vCard columns */
     private static final ProjectionMap sContactsVCardProjectionMap = ProjectionMap.builder()
             .add(Contacts._ID)
+            .add(Contacts.DISPLAY_NAME_PRIMARY)
+            .add(Contacts.DISPLAY_NAME_ALTERNATIVE)
             .add(OpenableColumns.DISPLAY_NAME, Contacts.DISPLAY_NAME + " || '.vcf'")
             .add(OpenableColumns.SIZE, "NULL")
             .build();
@@ -1957,6 +1959,10 @@ public class ContactsProvider2 extends AbstractContactsProvider
         // Enforce stream items access check if applicable.
         enforceSocialStreamWritePermission(uri);
 
+        if (getContext().isPrivacyGuardEnabled()) {
+            return null;
+        }
+
         if (mapsToProfileDbWithInsertedValues(uri, values)) {
             switchToProfileMode();
             return mProfileProvider.insert(uri, values);
@@ -1981,6 +1987,10 @@ public class ContactsProvider2 extends AbstractContactsProvider
         // Enforce stream items access check if applicable.
         enforceSocialStreamWritePermission(uri);
 
+        if (getContext().isPrivacyGuardEnabled()) {
+            return 0;
+        }
+
         if (mapsToProfileDb(uri)) {
             switchToProfileMode();
             return mProfileProvider.update(uri, values, selection, selectionArgs);
@@ -1996,6 +2006,10 @@ public class ContactsProvider2 extends AbstractContactsProvider
 
         // Enforce stream items access check if applicable.
         enforceSocialStreamWritePermission(uri);
+
+        if (getContext().isPrivacyGuardEnabled()) {
+            return 0;
+        }
 
         if (mapsToProfileDb(uri)) {
             switchToProfileMode();
@@ -4762,6 +4776,21 @@ public class ContactsProvider2 extends AbstractContactsProvider
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
             String sortOrder, CancellationSignal cancellationSignal) {
+        Cursor c = queryInternal(uri, projection, selection, selectionArgs,
+                sortOrder, cancellationSignal);
+
+        if (getContext().isPrivacyGuardEnabled()) {
+            Log.d(TAG, "Contacts query from application with privacy guard! pid=" + Binder.getCallingPid());
+            MemoryCursor mc = new MemoryCursor(null, c.getColumnNames());
+            c.close();
+            return mc;
+        }
+
+        return c;
+    }
+
+    private Cursor queryInternal(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                String sortOrder, CancellationSignal cancellationSignal) {
         if (VERBOSE_LOGGING) {
             Log.v(TAG, "query: uri=" + uri + "  projection=" + Arrays.toString(projection) +
                     "  selection=[" + selection + "]  args=" + Arrays.toString(selectionArgs) +
